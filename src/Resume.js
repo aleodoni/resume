f57f4a7ef7ec4c5f501cc8bc07a5ec7726235c31
aleodoni <aleodoni@gmail.com>
2019-08-15 14:20:29 -0300

    added courses

--- src/Resume.js
+++ src/Resume.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import About from './About';
-import Education from './Education';
-import Work from './Work';
-import Other from './Other';
+import React, { Component } from "react";
+import About from "./About";
+import Education from "./Education";
+import Work from "./Work";
+import Other from "./Other";
+import Courses from "./Courses";
 
-import data from './data/data.json';
-import './Resume.css';
+import data from "./data/data.json";
+import "./Resume.css";
 
 class Resume extends Component {
   render() {
     return (
       <div className="row">
-        <About data={data.about}/>
+        <About data={data.about} />
         <section className="col s12 m12 l8 section">
           <div className="row">
-            <Work data={data.work}/>
-            <Education data={data.education}/>
-            <Courses data={data.courses}/>
-            <Other data={data.other}/>
+            <Work data={data.work} />
+            <Education data={data.education} />
+            <Courses data={data.courses} />
+            <Other data={data.other} />
           </div>
         </section>
       </div>
